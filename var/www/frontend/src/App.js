fab9ba5411f4f096f0a07c700af925dc5ddc0748
Shinnosuke Iino <sengoku731sin@gmail.com>
2021-02-03 21:14:18 +0900

    Edit .gitignore

--- var/www/frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './reset.css';
-import Routers from './components/Routers';
-import Test from './Test';
-
-// ReactDOM.render(<Test />, document.getElementById('app'));
-ReactDOM.render(<Routers />, document.getElementById('app'));
